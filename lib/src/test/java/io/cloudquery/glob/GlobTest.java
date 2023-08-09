bc40b1054b98bdaf54d16bbb1eb1313c803d6995
Martin Norbury <martin.norbury@gmail.com>
2023-08-09 11:43:54 +0100

    chore: refactor to use same `glob` as Go (#23)
    Implement the `Glob` class in Java to be the same a [Go](https://github.com/cloudquery/plugin-sdk/blob/main/glob/glob.go) to make sure we have parity between Java and Go with the globbing behaviour.

---
+++ lib/src/test/java/io/cloudquery/glob/GlobTest.java
@@ -0,0 +1,100 @@
+package io.cloudquery.glob;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static io.cloudquery.glob.Glob.GLOB;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class GlobTest {
+    @Test
+    public void testEmptyPattern() {
+        assertGlobMatch("", "");
+        assertNotGlobMatch("", "test");
+    }
+
+    @Test
+    public void testEmptySubject() {
+        for (String s : List.of("",
+                "*",
+                "**",
+                "***",
+                "****************",
+                GLOB.repeat(1000000)
+        )) {
+            assertGlobMatch(s, "");
+        }
+
+        for (String pattern : List.of(
+                // No globs/non-glob characters
+                "test",
+                "*test*",
+
+                // Trailing characters
+                "*x",
+                "*****************x",
+                GLOB.repeat(1000000) + "x",
+
+                // Leading characters
+                "x*",
+                "x*****************",
+                "x" + GLOB.repeat(1000000),
+
+                // Mixed leading/trailing characters
+                "x*x",
+                "x****************x",
+                "x" + GLOB.repeat(1000000) + "x"
+        )) {
+            assertNotGlobMatch(pattern, "");
+        }
+    }
+
+    @Test
+    public void testPatternWithoutGlobs() {
+        assertGlobMatch("test", "test");
+    }
+
+    @Test
+    public void testGlobs() {
+        for (String pattern : List.of(
+                "*test",           // Leading glob
+                "this*",           // Trailing glob
+                "this*test",       // Middle glob
+                "*is *",           // String in between two globs
+                "*is*a*",          // Lots of globs
+                "**test**",        // Double glob characters
+                "**is**a***test*", // Varying number of globs
+                "* *",             // White space between globs
+                "*",               // Lone glob
+                "**********",      // Nothing but globs
+                "*Ѿ*",             // Unicode with globs
+                "*is a ϗѾ *"       // Mixed ASCII/unicode
+        )) {
+            assertGlobMatch(pattern, "this is a ϗѾ test");
+        }
+
+        for(String pattern:List.of(
+                "test*",               // Implicit substring match
+                "*is",                 // Partial match
+                "*no*",                // Globs without a match between them
+                " ",                   // Plain white space
+                "* ",                  // Trailing white space
+                " *",                  // Leading white space
+                "*ʤ*",                 // Non-matching unicode
+                "this*this is a test"  // Repeated prefix
+        )) {
+            assertNotGlobMatch(pattern, "this is a test");
+        }
+    }
+
+    public void assertGlobMatch(String pattern, String subject) {
+        assertTrue(String.format("\"%s\" should match \"%s\"", pattern, subject), Glob.match(pattern, subject));
+    }
+
+    public void assertNotGlobMatch(String pattern, String subject) {
+        assertFalse(String.format("\"%s\" should not match \"%s\"", pattern, subject), Glob.match(pattern, subject));
+    }
+
+}
