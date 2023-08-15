1270607f3bbca49c63eabf021bac410b9914cd21
candiduslynx <candiduslynx@gmail.com>
2023-08-15 10:35:03 +0300

    impl

---
+++ lib/src/main/java/io/cloudquery/scalar/Timestamp.java
@@ -0,0 +1,113 @@
+package io.cloudquery.scalar;
+
+import org.apache.arrow.vector.types.TimeUnit;
+import org.apache.arrow.vector.types.pojo.ArrowType;
+
+import java.time.*;
+
+public class Timestamp implements Scalar {
+    public static final ZoneId zoneID = ZoneOffset.UTC.normalized();
+
+    // TODO: add more units support later
+    private static final ArrowType dt = new ArrowType.Timestamp(TimeUnit.MILLISECOND, zoneID.toString());
+
+    protected ZonedDateTime value;
+
+    public Timestamp() {
+    }
+
+    public Timestamp(Object value) throws ValidationException {
+        this.set(value);
+    }
+
+    @Override
+    public String toString() {
+        if (this.value != null) {
+            return this.value.toString();
+        }
+        return NULL_VALUE_STRING;
+    }
+
+    @Override
+    public boolean isValid() {
+        return this.value != null;
+    }
+
+    @Override
+    public ArrowType dataType() {
+        return dt;
+    }
+
+    @Override
+    public void set(Object value) throws ValidationException {
+        if (value == null) {
+            this.value = null;
+            return;
+        }
+
+        if (value instanceof Scalar scalar) {
+            if (!scalar.isValid()) {
+                this.value = null;
+                return;
+            }
+
+            if (scalar instanceof Timestamp Timestamp) {
+                this.value = Timestamp.value;
+                return;
+            }
+
+            this.set(scalar.get());
+            return;
+        }
+
+        if (value instanceof ZonedDateTime timestamp) {
+            this.value = timestamp.withZoneSameInstant(zoneID);
+            return;
+        }
+
+        if (value instanceof LocalDate date) {
+            this.value = date.atStartOfDay(zoneID);
+            return;
+        }
+
+        if (value instanceof LocalDateTime date) {
+            this.value = date.atZone(zoneID);
+            return;
+        }
+
+        if (value instanceof Integer integer) {
+            this.value = ZonedDateTime.ofInstant(Instant.ofEpochMilli(integer), ZoneOffset.UTC.normalized());
+            return;
+        }
+
+        if (value instanceof Long longValue) {
+            this.value = ZonedDateTime.ofInstant(Instant.ofEpochMilli(longValue), ZoneOffset.UTC.normalized());
+            return;
+        }
+
+        if (value instanceof CharSequence sequence) {
+            this.value = ZonedDateTime.parse(sequence);
+            return;
+        }
+
+        throw new ValidationException(ValidationException.NO_CONVERSION_AVAILABLE, this.dataType(), value);
+    }
+
+    @Override
+    public Object get() {
+        return this.value; // null or proper value
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+
+        if (!(other instanceof Timestamp o)) {
+            return false;
+        }
+
+        return this.value == o.value || this.value.equals(o.value);
+    }
+}
