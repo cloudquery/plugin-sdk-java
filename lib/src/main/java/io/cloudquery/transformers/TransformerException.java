ef7c6131f180dd9b1df977397b0a606bc236a746
Martin Norbury <martin.norbury@gmail.com>
2023-08-15 08:55:14 +0100

    chore: adding a resolver transformer (#43)
    Adding the default implementation for the `ResolverTransformer` to set the value of a scalar based on a path lookup in the base resource item.
    
    fixes: #32

--- lib/src/main/java/io/cloudquery/transformers/TransformerException.java
+++ lib/src/main/java/io/cloudquery/transformers/TransformerException.java
@@ -1,7 +1,11 @@
 package io.cloudquery.transformers;
 
-public class TransformerException extends Exception{
+public class TransformerException extends Exception {
     public TransformerException(String message) {
         super(message);
+    }
+
+    public TransformerException(String message, Throwable ex) {
+        super(message, ex);
     }
 }
